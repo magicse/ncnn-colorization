e5973affe97f54775cb3e7aa74146d61cbc1e333
magicse <magicse@users.noreply.github.com>
2022-09-03 21:17:52 +0300

    Create min.cpp

---
+++ min.cpp
@@ -0,0 +1,13 @@
+#include <omp.h>
+#include <string>
+#include <vector>
+#include <ostream>
+#include <random>
+#include <chrono>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <face.h>
