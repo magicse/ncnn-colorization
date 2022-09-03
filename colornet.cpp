e823dffec79bb5b2c6e5d3fe50023d4a317c54c9
magicse <magicse@users.noreply.github.com>
2022-09-03 21:41:39 +0300

    Update colornet.cpp

--- colornet.cpp
+++ colornet.cpp
@@ -1,1 +1,20 @@
+#include <omp.h>
+#include "net.h"
+#include <iostream>
+#include <iomanip>
 
+#if defined(USE_NCNN_SIMPLEOCV)
+#include "simpleocv.h"
+#else
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#endif
+#include <stdio.h>
+#include <vector>
+
+static int detect_scrfd(const cv::Mat& bgr, const cv::Mat& out_image)
+{
+  
+}
