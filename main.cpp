cd00856e4d614285a1dd612772b59eedfcc91629
magicse <magicse@users.noreply.github.com>
2022-09-03 21:21:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,4 +10,40 @@
 
 #include <fstream>
 #include <opencv2/opencv.hpp>
-#include <face.h>
+#include <colornet.h>
+
+int main(int argc, char** argv)
+{
+    int opt = 0;
+    char *in_fname = NULL;
+    char *out_fname = NULL;
+
+    while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+            switch(opt){
+                case 'i':
+                    in_fname = optarg;
+                    printf("\nInput option value=%s", in_fname);
+                    break;
+                case 'o':
+                    out_fname = optarg;
+                    printf("\nOutput option value=%s", out_fname);
+                    break;
+                case '?':
+                    if (optopt == 'i'){
+                        printf("\nMissing input file name");
+                    } else if (optopt == 'o') {
+                         printf("\nMissing output file name");
+                    } else {
+                        printf("\nInvalid option received");
+                        printf("\nUsage: COLOR_GAN -i input file name -o output file name");
+                    }
+                    break;
+            }
+
+    }
+
+    char *argv_my[] = {"bw_imge", in_fname, NULL };
+    main_test(2, argv_my);
+printf("\nFinal");
+return 0;
+}
